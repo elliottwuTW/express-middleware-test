67229e64cf23e31e5585fbb06400e3962749f3c8
elliottwuTW <elliott10009@gmail.com>
2020-10-24 14:00:48 +0800

    fix: modify the resopnse time record

--- app.js
+++ app.js
@@ -8,31 +8,30 @@ const faviconPreventHTML = '<link rel="icon" href="data:;base64,=">'
 app.use((req, res, next) => {
   req.time = new Date()
   console.log(`${req.time.toISOString().slice(0, 10)} ${req.time.getHours()}:${req.time.getMinutes()}:${req.time.getSeconds()} | ${req.method} from ${req.originalUrl}`)
+  const startTime = process.hrtime()
   next()
+
+  // log the request-response elapsed time
+  res.on('finish', () => {
+    const duration = process.hrtime(startTime)
+    console.log('Duration: ', (duration[0] * 1e3 + duration[1] * 1e-6).toFixed(4), ' ms')
+  })
 })
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.send(faviconPreventHTML + '列出全部 Todo')
-  next()
 })
 
-app.get('/new', (req, res, next) => {
+app.get('/new', (req, res) => {
   res.send(faviconPreventHTML + '新增 Todo 頁面')
-  next()
 })
 
-app.get('/:id', (req, res, next) => {
+app.get('/:id', (req, res) => {
   res.send(faviconPreventHTML + '顯示一筆 Todo')
-  next()
 })
 
-app.post('/', (req, res, next) => {
+app.post('/', (req, res) => {
   res.send(faviconPreventHTML + '新增一筆  Todo')
-  next()
-})
-
-app.use((req, res) => {
-  console.log('Duration: ', Date.now() - req.time, ' ms')
 })
 
 app.listen(port, () => {
